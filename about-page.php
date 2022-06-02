479cbd48a9085cc60154407d2cc752ef292d52a7
unknown <dhanna@alphaworks.tech>
2022-06-02 15:57:29 -0700

    Add Contact and About pages

---
+++ about-page.php
@@ -0,0 +1,14 @@
+<?php 
+/** 
+ * Template Name: About 
+ * 
+ * @package TravelSite 
+ *  
+ */ 
+?> 
+<?php 
+get_header();
+?>
+<?php 
+get_footer();
+?>
---
+++ contact-page.php
@@ -0,0 +1,14 @@
+<?php 
+/** 
+ * Template Name: contact 
+ * 
+ * @package TravelSite 
+ *  
+ */ 
+?> 
+<?php 
+get_header();
+?>
+<?php 
+get_footer();
+?>
